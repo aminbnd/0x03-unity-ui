7bd817a07f36b5c07bdaadd7b32ce0945f7c2b33
aminbnd <2212@holbertonschool.com>
2021-08-20 17:49:02 +0200

    First commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 1000f;
+    public Rigidbody rb;
+    private int score = 0;
+    public int health = 5;
+    
+
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Pickup")
+        {
+            score++;
+            Debug.Log($"Score: {score}");
+            Destroy(other.gameObject);
+        }
+        
+        if(other.tag == "Trap")
+        {
+            health--;
+            Debug.Log($"Health: {health}");
+        }
+
+        if(other.tag == "Goal")
+        {
+            Debug.Log("You win!");
+        }
+    }
+    
+    
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(Input.GetKey("w") || Input.GetKey("up"))
+        {
+            rb.AddForce(0, 0, speed * Time.deltaTime);
+        }
+
+        if(Input.GetKey("s") || Input.GetKey("down"))
+        {
+            rb.AddForce(0, 0, -speed * Time.deltaTime);
+        }
+
+        if(Input.GetKey("a") || Input.GetKey("left"))
+        {
+            rb.AddForce(-speed * Time.deltaTime, 0, 0);
+        }
+
+        if(Input.GetKey("d") || Input.GetKey("right"))
+        {
+            rb.AddForce(speed * Time.deltaTime,0,0);
+        }
+    }
+
+
+    void Update()
+    {
+        if(health == 0)
+        {
+            Debug.Log("Game Over!");
+            SceneManager.LoadScene(0, LoadSceneMode.Single);
+        }
+    }
+}
