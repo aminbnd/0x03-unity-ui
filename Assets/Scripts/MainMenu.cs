9946a947306e2ea2ea9b32806f6086da7e150dd1
aminbnd <2212@holbertonschool.com>
2021-08-25 15:54:38 +0200

    Task 12

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,23 +6,24 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-   // public Material trapMat;
-    // public Material goalMat;
-    //public Toggle colorblindMode;
+    public Material trapMat;
+    public Material goalMat;
+    public Toggle colorblindMode;
+
 
     // Load the maze scene when the play button is pressed 
     public void PlayMaze()
     {
-        // if (colorblindMode.isOn)
-        // {
-        //     trapMat.color = new Color32(255, 112, 0, 1);
-        //     goalMat.color = Color.blue;
-        // }
-        // else
-        // {
-        //     trapMat.color = Color.red;
-        //     goalMat.color = Color.green;
-        // }
+        if (colorblindMode.isOn)
+        {
+            trapMat.color = new Color32(255, 112, 0, 1);
+            goalMat.color = Color.blue;
+        }
+        else
+        {
+            trapMat.color = Color.red;
+            goalMat.color = Color.green;
+        }
 
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
